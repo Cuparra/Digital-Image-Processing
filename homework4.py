47bf5652597518d0e3447f7789a911797e573e63
Tiago Moreira Trocoli da Cunha <ra226078@scooby.lab.ic.unicamp.br>
2019-06-08 14:22:13 -0300

    Working on homework4 2

--- homework4.py
+++ homework4.py
@@ -11,69 +11,56 @@ Created on Fri Jun  7 13:24:30 2019
 # python 3
 
 import numpy as np
-import cv2 as cv2
+import cv2
 from matplotlib import pyplot as plt
 
 
-#Load Images and converting them into grayscale
-def load_images(image1, image2):
-    
-    img1 = cv2.imread(image1)
-    img2 = cv2.imread(image2)
-    gray1 = cv2.imread(image1,cv2.IMREAD_GRAYSCALE)
-    gray2 = cv2.imread(image2,cv2.IMREAD_GRAYSCALE)
-    
-    return (img1, img2, gray1, gray2)
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
+def match_detector(image1,image2,detector):
+    
+    kp1, des1 = detector.detectAndCompute(image1,None)
+    kp2, des2 = detector.detectAndCompute(image2,None)
 
-# find the k best matches from one image to another
-def find_matches(gray1, gray2, des1, des2, kp1, kp2, min_matches):
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1,des2, k=2)
-    # Apply ratio test
+
     good = []
-    for m in matches:
-        if m[0].distance < 0.75*m[1].distance:
-            good.append(m)
-            matches = np.asarray(good)
-    if len(matches[:,0]) >= 4:
-        # cv2.drawMatchesKnn expects list of lists as matches.
-        img3 = cv2.drawMatchesKnn(gray1,kp1,gray2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        cv2.imwrite("output.jpg",img3)
-    
-    
+    good_without_list = []
 
-def join_show_images(image1, image2, H):
+    for m, n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append([m])
+            good_without_list.append(m)
+    imageWithMatches = cv2.drawMatchesKnn(image2,kp2,image1,kp1,good,None,flags=2)
+
+    match = np.asarray(good)
+    if len(match[:,0]) >= 4:
+        src = np.float32([ kp1[m.queryIdx].pt for m in match[:,0] ]).reshape(-1,1,2)
+        dst = np.float32([ kp2[m.trainIdx].pt for m in match[:,0] ]).reshape(-1,1,2)
+
+    H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+    print(H)
     dst = cv2.warpPerspective(image1,H,(image2.shape[1] + image1.shape[1], image2.shape[0]))
-    plt.subplot(122)
-    plt.imshow(dst)
-    plt.title("Warped Image")
-    plt.show()
-    plt.figure()
-    dst[0:image2.shape[0], 0:image2.shape[1]] = image2
-    cv2.imwrite("output.jpg",dst)
-    plt.imshow(dst)
-    plt.show()
+    
+    dst[0:image1.shape[0], 0:image1.shape[1]] = image2	
+    return imageWithMatches,dst
 
-# SIFT algorithm
-def scale_invariante_feature(image1, image2):
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(image1,None)
-    kp2, des2 = sift.detectAndCompute(image2,None)
-    return kp1, kp2, des1, des2
-
-# SURF algorithm
-def speeded_up_robust_features(image1, image2):
-    surf = cv2.xfeatures2d.SURF_create()
-    kp1, des1 = surf.detectAndCompute(image1,None)
-    kp2, des2 = surf.detectAndCompute(image2,None)
-    return kp1, kp2, des1, des2
-
+def show_and_save(imageWithMatches,dst):
+	cv2.imwrite("matches.jpg", imageWithMatches)
+	plt.imshow(np.flip(imageWithMatches, axis=2))
+	plt.show()
+	
+	cv2.imwrite("output.jpg",dst)
+	plt.imshow(np.flip(dst, axis=2))
+	plt.show()
+	
 def main():
     
-    image1, image2, gray1, gray2 = load_images("foto1A.jpg", "foto1B.jpg")
-    kp1, kp2, des1, des2         = speeded_up_robust_features(image1, image2)
-    find_matches(gray1, gray2, des1, des2, kp1, kp2,10)
+    imageWithMatches,dst = match_detector("foto1A.jpg", "foto1B.jpg", cv2.xfeatures2d.SIFT_create())
+    show_and_save(imageWithMatches, dst)
     
 if __name__ == "__main__":
     main()
